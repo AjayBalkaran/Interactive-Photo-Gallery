75a9bf1fc351db24f5b92d0acb31ce0720d2a6fe
Ajay Balkaran <ajaybalkaran@gmail.com>
2019-11-05 22:28:13 -0500

    Update main.js
    * created search function for search bar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const searchBar = document.forms['search-img'].querySelector('input');
+
+$('#search-img').keyup(function() {
+    const $search = event.target.value.toLowerCase();
+    $('.chocolat-parent a').each(function() {
+        var $titleText = $(this).attr("title").toLowerCase();
+        if($titleText.search($search) > -1) {
+            $(this).parent().show();
+        } else {
+             $(this).parent().hide();
+        }
+    })
+});
+
 $(document).ready(function(){
     $('.chocolat-parent').Chocolat();
 });
+
